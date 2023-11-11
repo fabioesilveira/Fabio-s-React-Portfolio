6a4d2ed91afc334d611a96a3e1af5e2f2c088318
Fabio Silveira <fabio.eds@hotmail.com>
2023-11-11 10:30:18 -0800

    AddedSRC

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function AboutPage() {
+  return (
+    <div className="container pt-4">
+      <p>
+        something about me
+      </p>
+      
+      
+    </div>
+  );
+}
---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,11 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function ProjectsPage() {
+  return (
+    <div className="container pt-4">
+      <p>
+        Here are some of my projects I've worked on recently:
+      </p>
+      
+    </div>
+  );
+}
